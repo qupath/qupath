4fb99ca554377dc00fc3cf2305484d3cc24cdeab
pete <pete.bankhead@gmail.com>
2016-10-22 13:57:23 +0100

    Testing out Delaunay clustering with K-means

--- qupath-extension-opencv/src/main/java/qupath/opencv/features/DelaunayClusteringPlugin.java
+++ qupath-extension-opencv/src/main/java/qupath/opencv/features/DelaunayClusteringPlugin.java
@@ -28,12 +28,20 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.apache.commons.math3.stat.correlation.PearsonsCorrelation;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.TermCriteria;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import qupath.lib.common.ColorTools;
 import qupath.lib.common.GeneralTools;
 import qupath.lib.gui.QuPathGUI;
 import qupath.lib.gui.viewer.QuPathViewer;
@@ -43,11 +51,13 @@ import qupath.lib.objects.PathAnnotationObject;
 import qupath.lib.objects.PathObject;
 import qupath.lib.objects.PathRootObject;
 import qupath.lib.objects.TMACoreObject;
+import qupath.lib.objects.classes.PathClassFactory;
 import qupath.lib.objects.helpers.PathObjectTools;
 import qupath.lib.plugins.AbstractInteractivePlugin;
 import qupath.lib.plugins.PathTask;
 import qupath.lib.plugins.PluginRunner;
 import qupath.lib.plugins.parameters.ParameterList;
+import qupath.opencv.features.DefaultPathObjectConnections.MeasurementNormalizer;
 
 /**
  * Plugin for calculating Delaunay clustering, and associated features.
@@ -118,6 +128,7 @@ public class DelaunayClusteringPlugin extends AbstractInteractivePlugin<Buffered
 			viewer = null;
 			overlay = null;
 		}
+		pluginRunner.getHierarchy().fireHierarchyChangedEvent(this);
 	}
 	
 
@@ -221,7 +232,7 @@ public class DelaunayClusteringPlugin extends AbstractInteractivePlugin<Buffered
 		private boolean addClusterMeasurements;
 		private boolean limitByClass;
 		
-		private DelaunayTriangulation dt;
+		private PathObjectConnections result;
 		
 		private String lastResult = null;
 		
@@ -235,6 +246,7 @@ public class DelaunayClusteringPlugin extends AbstractInteractivePlugin<Buffered
 			this.overlay = overlay;
 		}
 
+		
 		@Override
 		public void run() {
 			
@@ -245,18 +257,258 @@ public class DelaunayClusteringPlugin extends AbstractInteractivePlugin<Buffered
 				return;
 			}
 			
-			dt = new DelaunayTriangulation(pathObjects, pixelWidth, pixelHeight, distanceThresholdPixels, limitByClass);
+			MeasurementNormalizer normalizer = new MeasurementNormalizer(pathObjects);
+			List<String> measurements = new ArrayList<>(normalizer.getAvailableMeasurements());
+			
+			measurements = measurements.stream().filter(p -> {
+				return (p.toLowerCase().contains("haralick") || p.toLowerCase().contains("smooth")) && !p.toLowerCase().contains("cluster");
+//				return !p.toLowerCase().contains("cluster");
+			}).collect(Collectors.toList());
+//			measurements = measurements.stream().filter(p -> {
+//				return p.toLowerCase().contains("haralick") && !p.toLowerCase().contains("cluster");
+//			}).collect(Collectors.toList());
+			
+//			double correlationThreshold = 0.8;
+//			List<String> measurementsToRemove = new ArrayList<>();
+//			PearsonsCorrelation corr = new PearsonsCorrelation();
+//			for (int i = 0; i < measurements.size(); i++) {
+//				String namei = measurements.get(i);
+//				double[] xArray = pathObjects.stream().mapToDouble(p -> p.getMeasurementList().getMeasurementValue(namei)).toArray();
+//				for (int j = i+1; j < measurements.size(); j++) {
+//					String namej = measurements.get(j);
+//					double[] yArray = pathObjects.stream().mapToDouble(p -> p.getMeasurementList().getMeasurementValue(namej)).toArray();
+//					double pcc = Math.abs(corr.correlation(xArray, yArray));
+//					if (pcc > correlationThreshold) {
+//						measurementsToRemove.add(namej);
+//						break;
+//					}
+//				}
+//			}
+//			System.err.println("Remove: " + measurementsToRemove.size());
+//			measurements.removeAll(measurementsToRemove);
+			
+						
+			int k = 4;
+			int attempts = 1;
+			Mat data = new Mat(pathObjects.size(), measurements.size(), CvType.CV_32F);
+			
+			double[] values = new double[measurements.size()];
+			for (int i = 0; i < pathObjects.size(); i++) {
+				values = normalizer.normalizeMeanStdDev(pathObjects.get(i), measurements, 0, values);
+				data.put(i, 0, values);
+			}
+			
+			Mat bestLabels = new Mat(pathObjects.size(), 1, CvType.CV_32S);
+			TermCriteria termCriteria = new TermCriteria(TermCriteria.COUNT + TermCriteria.EPS, 100, 0.001);
+			Core.kmeans(data, k, bestLabels, termCriteria, attempts, Core.KMEANS_PP_CENTERS);
+			
+//			Mat centers = new Mat(k, measurements.size(), CvType.CV_32F);
+//			Core.kmeans(data, k, bestLabels, termCriteria, attempts, Core.KMEANS_PP_CENTERS, centers);
+//			centers.release();
+			
+			Map<PathObject, Integer> mapLabels = new HashMap<>();
+			int[] label = new int[1];
+			for (int i = 0; i < pathObjects.size(); i++) {
+				bestLabels.get(i, 0, label);
+				mapLabels.put(pathObjects.get(i), label[0]);
+			}
+			
+			data.release();
+			bestLabels.release();
+			
+			
+			// Move through and break connections
+			for (PathObject pathObject : pathObjects) {
+				int currentLabel = mapLabels.get(pathObject);
+				// Set PathClass
+				pathObject.setPathClass(
+						PathClassFactory.getPathClass("Cluster " + currentLabel,
+								ColorTools.makeRGB((int)(Math.random() * 256), (int)(Math.random() * 256), (int)(Math.random() * 256))
+								)
+						);
+			}
+			
+			DelaunayTriangulation dt = new DelaunayTriangulation(pathObjects, pixelWidth, pixelHeight, distanceThresholdPixels, limitByClass);
+			
+			DefaultPathObjectConnections result = new DefaultPathObjectConnections(dt);
+			pathObjects = new ArrayList<>(result.getPathObjects());
+
+			
+			
+//			result.sortByDistance();
+//			RunningStatistics stats = new RunningStatistics();
+//			RunningStatistics statsRandom = new RunningStatistics();
+//			for (PathObject pathObject : pathObjects) {
+//				List<PathObject> connections = result.getConnectedObjects(pathObject);
+//				if (connections.isEmpty())
+//					continue;
+//				normalizer.
+//				double distance = normalizer.getMeasurementDistance(pathObject, connections.get(0), measurements);				
+////				double distance = normalizer.getMeasurementDistance(pathObject, connections.get(connections.size()/2), measurements);				
+//				stats.addValue(distance);
+//				
+//				distance = normalizer.getMeasurementDistance(pathObject, pathObjects.get((int)(Math.random() * pathObjects.size())), measurements);				
+//				statsRandom.addValue(distance);
+//			}
+//			
+//			System.err.println("Mean: " + stats.getMean());
+//			System.err.println("Std dev: " + stats.getStdDev());
+//
+//			System.err.println("Random Mean: " + statsRandom.getMean());
+//			System.err.println("Random Std dev: " + statsRandom.getStdDev());
+//
+//			// Determine connection-breaking threshold
+//			double threshold = stats.getMean();// + stats.getStdDev();
+//			
+//			// Work with squared values now
+//			threshold = threshold*threshold;
+//
+//			// Move through and break connections
+//			for (PathObject pathObject : pathObjects) {
+//				List<PathObject> connections = result.getConnectedObjects(pathObject);
+//				if (connections.isEmpty())
+//					continue;
+//				for (PathObject connection : connections.toArray(new PathObject[0])) {
+//					double distanceSq = normalizer.getMeasurementDistanceSquared(pathObject, connections.get(connections.size()/2), measurements);
+//					if (distanceSq > threshold)
+//						result.breakConnection(pathObject, connection);
+//				}
+//			}
+
+			
 			dt.addNodeMeasurements();
 			if (addClusterMeasurements)
 				dt.addClusterMeasurements();
 			
+			
+			this.result = result;
+			
 			lastResult = "Delaunay triangulation calculated for " + parentObject;
 		}
+		
+		
+//		@Override
+//		public void run() {
+//			
+//			List<PathObject> pathObjects = PathObjectTools.getFlattenedObjectList(parentObject, null, false);
+//			pathObjects = pathObjects.stream().filter(p -> p.isDetection()).collect(Collectors.toList());
+//			if (pathObjects.isEmpty()) {
+//				lastResult = "No detection descendant objects for " + parentObject;
+//				return;
+//			}
+//			
+//			DelaunayTriangulation dt = new DelaunayTriangulation(pathObjects, pixelWidth, pixelHeight, distanceThresholdPixels, limitByClass);
+//			
+//			DefaultPathObjectConnections result = new DefaultPathObjectConnections(dt);
+//			pathObjects = new ArrayList<>(result.getPathObjects());
+//			MeasurementNormalizer normalizer = new MeasurementNormalizer(pathObjects);
+//			List<String> measurements = new ArrayList<>(normalizer.getAvailableMeasurements());
+//			
+//			measurements = measurements.stream().filter(p -> p.toLowerCase().contains("haralick")).collect(Collectors.toList());
+//			
+//			int k = 3;
+//			int attempts = 5;
+//			Mat data = new Mat(pathObjects.size(), measurements.size(), CvType.CV_32F);
+//			
+//			double[] values = new double[measurements.size()];
+//			for (int i = 0; i < pathObjects.size(); i++) {
+//				values = normalizer.normalizeMeanStdDev(pathObjects.get(i), measurements, 0, values);
+//				data.put(i, 0, values);
+//			}
+//			
+//			Mat bestLabels = new Mat(pathObjects.size(), 1, CvType.CV_32S);
+//			TermCriteria termCriteria = new TermCriteria(TermCriteria.COUNT + TermCriteria.EPS, 100, 0.001);
+//			Core.kmeans(data, k, bestLabels, termCriteria, attempts, Core.KMEANS_PP_CENTERS);
+//			
+//			Map<PathObject, Integer> mapLabels = new HashMap<>();
+//			int[] label = new int[1];
+//			for (int i = 0; i < pathObjects.size(); i++) {
+//				bestLabels.get(i, 0, label);
+//				mapLabels.put(pathObjects.get(i), label[0]);
+//			}
+//			
+//			data.release();
+//			bestLabels.release();
+//			
+//			
+//			// Move through and break connections
+//			for (PathObject pathObject : pathObjects) {
+//				List<PathObject> connections = result.getConnectedObjects(pathObject);
+//				if (connections.isEmpty())
+//					continue;
+//				int currentLabel = mapLabels.get(pathObject);
+//				for (PathObject connection : connections.toArray(new PathObject[0])) {
+//					int connectionLabel = mapLabels.get(connection);
+//					if (currentLabel != connectionLabel)
+//						result.breakConnection(pathObject, connection);
+//				}
+//				
+//				
+//				// Set PathClass while we're here (why not...?)
+//				pathObject.setPathClass(
+//						PathClassFactory.getPathClass("Cluster " + currentLabel,
+//								ColorTools.makeRGB((int)(Math.random() * 256), (int)(Math.random() * 256), (int)(Math.random() * 256))
+//								)
+//						);
+//			}
+//			
+//			
+//			
+////			result.sortByDistance();
+////			RunningStatistics stats = new RunningStatistics();
+////			RunningStatistics statsRandom = new RunningStatistics();
+////			for (PathObject pathObject : pathObjects) {
+////				List<PathObject> connections = result.getConnectedObjects(pathObject);
+////				if (connections.isEmpty())
+////					continue;
+////				normalizer.
+////				double distance = normalizer.getMeasurementDistance(pathObject, connections.get(0), measurements);				
+//////				double distance = normalizer.getMeasurementDistance(pathObject, connections.get(connections.size()/2), measurements);				
+////				stats.addValue(distance);
+////				
+////				distance = normalizer.getMeasurementDistance(pathObject, pathObjects.get((int)(Math.random() * pathObjects.size())), measurements);				
+////				statsRandom.addValue(distance);
+////			}
+////			
+////			System.err.println("Mean: " + stats.getMean());
+////			System.err.println("Std dev: " + stats.getStdDev());
+////
+////			System.err.println("Random Mean: " + statsRandom.getMean());
+////			System.err.println("Random Std dev: " + statsRandom.getStdDev());
+////
+////			// Determine connection-breaking threshold
+////			double threshold = stats.getMean();// + stats.getStdDev();
+////			
+////			// Work with squared values now
+////			threshold = threshold*threshold;
+////
+////			// Move through and break connections
+////			for (PathObject pathObject : pathObjects) {
+////				List<PathObject> connections = result.getConnectedObjects(pathObject);
+////				if (connections.isEmpty())
+////					continue;
+////				for (PathObject connection : connections.toArray(new PathObject[0])) {
+////					double distanceSq = normalizer.getMeasurementDistanceSquared(pathObject, connections.get(connections.size()/2), measurements);
+////					if (distanceSq > threshold)
+////						result.breakConnection(pathObject, connection);
+////				}
+////			}
+//
+//			
+//			dt.addNodeMeasurements();
+//			if (addClusterMeasurements)
+//				dt.addClusterMeasurements();
+//			
+//			
+//			this.result = result;
+//			
+//			lastResult = "Delaunay triangulation calculated for " + parentObject;
+//		}
 
 		@Override
 		public void taskComplete() {
-			if (dt != null && overlay != null)
-				overlay.addDelaunay(dt);
+			if (result != null && overlay != null)
+				overlay.addDelaunay(result);
 		}
 
 		@Override
--- qupath-processing-opencv/src/main/java/qupath/opencv/features/DefaultPathObjectConnections.java
+++ qupath-processing-opencv/src/main/java/qupath/opencv/features/DefaultPathObjectConnections.java
@@ -3,10 +3,13 @@ package qupath.opencv.features;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import qupath.lib.analysis.stats.RunningStatistics;
+import qupath.lib.measurements.MeasurementList;
 import qupath.lib.objects.PathObject;
 import qupath.lib.objects.helpers.PathObjectTools;
 import qupath.lib.roi.interfaces.ROI;
@@ -17,6 +20,10 @@ public class DefaultPathObjectConnections implements PathObjectConnections {
 	
 	public DefaultPathObjectConnections(final Collection<PathObject> pathObjects) {
 		pathObjects.stream().forEach(p -> map.put(p, new ObjectConnector(p)));
+	}
+	
+	public DefaultPathObjectConnections(final PathObjectConnections connections) {
+		connections.getPathObjects().stream().forEach(p -> map.put(p, new ObjectConnector(p, connections.getConnectedObjects(p))));
 	}
 	
 	public void addConnection(final PathObject pathObject1, final PathObject pathObject2) {
@@ -80,6 +87,78 @@ public class DefaultPathObjectConnections implements PathObjectConnections {
 	}
 	
 	
+	
+	public static class MeasurementNormalizer {
+		
+		static Map<String, RunningStatistics> statsMap = new HashMap<>();
+		
+		public MeasurementNormalizer(final Collection<PathObject> pathObjects) {
+			for (PathObject pathObject : pathObjects) {
+				MeasurementList list = pathObject.getMeasurementList();
+				for (int i = 0; i < list.size(); i++) {
+					double value = list.getMeasurementValue(i);
+					if (Double.isNaN(value))
+						continue;
+					String name = list.getMeasurementName(i);
+					RunningStatistics stats = statsMap.get(name);
+					if (stats == null) {
+						stats = new RunningStatistics();
+						statsMap.put(name, stats);
+					}
+					stats.addValue(value);
+				}
+			}
+		}
+		
+		public Collection<String> getAvailableMeasurements() {
+			return statsMap.keySet();
+		}
+				
+		
+		public double[] normalizeMeanStdDev(final PathObject pathObject, final List<String> measurements, final double nanValue, double[] results) {
+			if (results == null || results.length < measurements.size())
+				results = new double[measurements.size()];
+			
+			MeasurementList list = pathObject.getMeasurementList();
+			for (int i = 0; i < measurements.size(); i++) {
+				String name = measurements.get(i);
+				RunningStatistics stats = statsMap.get(name);
+				double val = list.getMeasurementValue(name);
+				if (Double.isNaN(val))
+					results[i] = nanValue;
+				else if (stats == null)
+					results[i] = val;
+				else
+					results[i] = (val - stats.getMean()) / stats.getStdDev();
+			}
+			
+			return results;
+		}
+		
+		
+		public double getMeasurementDistanceSquared(final PathObject pathObject1, final PathObject pathObject2, final List<String> measurements) {
+			// Get normalized values, replacing NaNs with zero (i.e. the mean)
+			double[] m1 = normalizeMeanStdDev(pathObject1, measurements, 0, null);
+			double[] m2 = normalizeMeanStdDev(pathObject2, measurements, 0, null);
+			
+			double sumSquares = 0;
+			for (int i = 0; i < measurements.size(); i++) {
+				double d = m1[i] - m2[i];
+				sumSquares += d*d;
+			}
+			
+			return sumSquares;
+		}
+
+		public double getMeasurementDistance(final PathObject pathObject1, final PathObject pathObject2, final List<String> measurements) {
+			return Math.sqrt(getMeasurementDistanceSquared(pathObject1, pathObject2, measurements));
+		}
+
+		
+	}
+	
+	
+	
 	static class ObjectConnector {
 		
 		private PathObject pathObject;
@@ -87,6 +166,11 @@ public class DefaultPathObjectConnections implements PathObjectConnections {
 		
 		ObjectConnector(final PathObject pathObject) {
 			this.pathObject = pathObject;
+		}
+		
+		ObjectConnector(final PathObject pathObject, final Collection<PathObject> connections) {
+			this.pathObject = pathObject;
+			this.connections.addAll(connections);
 		}
 		
 		public boolean addConnection(final PathObject newObject) {
