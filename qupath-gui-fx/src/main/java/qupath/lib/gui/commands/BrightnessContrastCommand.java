6367e9842bed33c0fbd4f4483491e5df4abe6f13
Pete <pete.bankhead@gmail.com>
2016-11-21 13:58:09 +0000

    Added tooltips to Brightness/Contrast Min/Max display labels

--- qupath-gui-fx/src/main/java/qupath/lib/gui/commands/BrightnessContrastCommand.java
+++ qupath-gui-fx/src/main/java/qupath/lib/gui/commands/BrightnessContrastCommand.java
@@ -150,9 +150,11 @@ public class BrightnessContrastCommand implements PathCommand, ImageDataChangeLi
 		
 		GridPane box = new GridPane();
 		Label labelMin = new Label("Min display");
+		labelMin.setTooltip(new Tooltip("Set minimum lookup table value - double-click to edit manually"));
 		box.add(labelMin, 0, 0);
 		box.add(sliderMin, 1, 0);
 		Label labelMax = new Label("Max display");
+		labelMax.setTooltip(new Tooltip("Set maximum lookup table value - double-click to edit manually"));
 		box.add(labelMax, 0, 1);
 		box.add(sliderMax, 1, 1);
 		box.setVgap(5);
