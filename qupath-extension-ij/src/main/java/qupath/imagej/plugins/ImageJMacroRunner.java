c1667c4106b15a3d29b500a70c31f65acfa76450
Olivier Burri <olivier.burri@epfl.ch>
2018-01-15 15:58:50 +0100

    Allows Macro interpreter to return annotations from the overlay, rather than just detections.

--- qupath-extension-ij/src/main/java/qupath/imagej/plugins/ImageJMacroRunner.java
+++ qupath-extension-ij/src/main/java/qupath/imagej/plugins/ImageJMacroRunner.java
@@ -218,7 +218,6 @@ public class ImageJMacroRunner extends AbstractPlugin<BufferedImage> {
 		double downsampleFactor = params.getDoubleParameterValue("downsampleFactor");
 		boolean sendROI = params.getBooleanParameterValue("sendROI");
 		boolean sendOverlay = params.getBooleanParameterValue("sendOverlay");
-
 		ROI pathROI = pathObject.getROI();		
 		ImageDisplay imageDisplay2 = Boolean.TRUE.equals(params.getBooleanParameterValue("useTransform")) ? imageDisplay : null;
 		RegionRequest region = RegionRequest.createInstance(imageData.getServer().getPath(), downsampleFactor, pathROI);
@@ -318,8 +317,10 @@ public class ImageJMacroRunner extends AbstractPlugin<BufferedImage> {
 						}
 					}
 				}
+				
+				boolean exportAsDetection = ((String) params.getChoiceParameterValue("getOverlayAs")).equals("Detections") ? true : false;
 				if (params.getBooleanParameterValue("getOverlay") && impResult.getOverlay() != null) {
-					List<PathObject> childObjects = QUPath_Send_Overlay_to_QuPath.createPathObjectsFromROIs(imp, impResult.getOverlay().toArray(), imageData.getServer(), downsampleFactor, true, true, -1, region.getZ(), region.getT());
+					List<PathObject> childObjects = QUPath_Send_Overlay_to_QuPath.createPathObjectsFromROIs(imp, impResult.getOverlay().toArray(), imageData.getServer(), downsampleFactor, exportAsDetection, true, -1, region.getZ(), region.getT());
 					if (!childObjects.isEmpty()) {
 						pathObject.addPathObjects(childObjects);
 						changes = true;
@@ -372,7 +373,8 @@ public class ImageJMacroRunner extends AbstractPlugin<BufferedImage> {
 				.addTitleParameter("Results")
 				.addBooleanParameter("clearObjects", "Clear current child objects", false)
 				.addBooleanParameter("getROI", "Create annotation from ImageJ ROI", false)
-				.addBooleanParameter("getOverlay", "Create detection objects from ImageJ overlay", false)
+				.addBooleanParameter("getOverlay", "Get objects from ImageJ overlay", false)
+				.addChoiceParameter("getOverlayAs", "Get objects as", "Detections", new String[]{"Detections", "Annotations"} )
 				;
 		return params;
 	}
@@ -434,6 +436,4 @@ public class ImageJMacroRunner extends AbstractPlugin<BufferedImage> {
 //		else
 //			return imageData.getHierarchy().getObjects(null, PathAnnotationObject.class);
 	}
-
-
 }
