c48e9798d126f101052b22c0da26662643b4adb5
pete <pete.bankhead@gmail.com>
2018-03-09 11:03:30 +0000

    Opacity slider now bound bidirectionally to viewer property
    This improves matters when using the cmd/ctrl + scroll shortcut to
    adjust opacity, where previously the slider would remain static

--- qupath-gui-fx/src/main/java/qupath/lib/gui/QuPathGUI.java
+++ qupath-gui-fx/src/main/java/qupath/lib/gui/QuPathGUI.java
@@ -4050,11 +4050,8 @@ public class QuPathGUI implements ModeWrapper, ImageDataWrapper<BufferedImage>, 
 			toolbar.getItems().add(qupath.getActionToggleButton(GUIActions.SHOW_OBJECTS, true, overlayOptions.getShowObjects()));
 			toolbar.getItems().add(qupath.getActionToggleButton(GUIActions.FILL_OBJECTS, true, overlayOptions.getFillObjects()));
 
-			final Slider sliderOpacity = new Slider(0, 20, (int)(overlayOptions.getOpacity()*20));
-			sliderOpacity.valueProperty().addListener(e -> {
-					double value = sliderOpacity.getValue() / (sliderOpacity.getMax() - sliderOpacity.getMin());
-					overlayOptions.setOpacity((float)value);
-			});
+			final Slider sliderOpacity = new Slider(0, 1, 1);
+			sliderOpacity.valueProperty().bindBidirectional(overlayOptions.opacityProperty());
 			sliderOpacity.setTooltip(new Tooltip("Overlay opacity"));
 			toolbar.getItems().add(sliderOpacity);
 			
