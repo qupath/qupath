f06510949824936bad4c62b933672d65c798dfaa
Tuatini Godard <tuatinigodard@gmail.com>
2018-06-06 15:30:46 +0200

    Improve the duplicate check process and fix a bug on mirax files import

--- qupath-extension-nn-converter/src/main/java/qupath/nn/commands/OpenFromRootPathCommand.java
+++ qupath-extension-nn-converter/src/main/java/qupath/nn/commands/OpenFromRootPathCommand.java
@@ -26,56 +26,62 @@ public class OpenFromRootPathCommand implements PathCommand {
     private QuPathGUI qupath;
     private static final String commandName = "Project: Import Mirax files";
     final private static Logger logger = LoggerFactory.getLogger(OpenFromRootPathCommand.class);
+    private Map<String, String> fileList;
+    private List<String> duplicates;
 
     public OpenFromRootPathCommand(final QuPathGUI qupath) {
         this.qupath = qupath;
     }
 
+    private void fillFileLists(List<Path> mrxsFiles) {
+        fileList = new HashMap<>();
+        duplicates = new ArrayList<>();
+
+        for (Path path : mrxsFiles) {
+            String fileName = path.getFileName().toString();
+            String abs_path = path.toAbsolutePath().toString();
+
+            // Check if the file doesn't have a duplicate name
+            if (fileList.keySet().contains(fileName)) {
+                duplicates.add(abs_path);
+            }
+            fileList.put(fileName, abs_path);
+        }
+    }
+
     private void openInProject(List<Path> mrxsFiles) {
         Map<String, ImageRetCode.IMAGE_CODE> retcodes = new HashMap<>();
-        Map<String, String> fileList = new HashMap<>();
-        List<String> duplicates = new ArrayList<>();
 
         Task<Void> worker = new Task<Void>() {
             @Override
-            protected Void call() throws Exception {
-                long max = mrxsFiles.size();
+            protected Void call() {
+                long max = fileList.size();
                 long counter = 0;
-                for (Path path : mrxsFiles) {
-                    String fileName = path.getFileName().toString();
-                    String abs_path = path.toAbsolutePath().toString();
+
+                for(Map.Entry<String, String> entry : fileList.entrySet()) {
+                    String fileName = entry.getKey();
+                    String abs_path = entry.getValue();
 
                     updateMessage(fileName);
                     updateProgress(counter, max);
-
-                    // Check if the file doesn't have a duplicate name
-                    if (fileList.keySet().contains(fileName)) {
-                        duplicates.add(abs_path);
-                    }
-                    fileList.put(fileName, abs_path);
 
                     ImageRetCode retCode = qupath.getProject().addImage(abs_path.trim());
 
                     if (!retCode.isOpenSlideImageServer()) {
                         DisplayHelpers.showErrorMessage("Error", "The mrsx files you opened is not using " +
                                 "OpenSlide image server. Please contact the developers about this issue.");
-                        break;
+                        return null;
                     }
-
                     retcodes.put(abs_path, retCode.getRetCode());
                     counter++;
+
                 }
                 updateProgress(max, max);
                 return null;
             }
         };
 
-        ProgressDialog progress = new ProgressDialog(worker);
-        progress.setWidth(500);
-        progress.setTitle("Importing Mirax files");
-
-        qupath.submitShortTask(worker);
-        progress.showAndWait();
+        fillFileLists(mrxsFiles);
 
         if (duplicates.size() > 0) {
             StringBuilder sb = new StringBuilder();
@@ -85,9 +91,17 @@ public class OpenFromRootPathCommand implements PathCommand {
 
             TextArea textArea = new TextArea();
             textArea.setText(sb.toString());
+
             DisplayHelpers.showErrorMessage(commandName, textArea);
             return;
         }
+
+        ProgressDialog progress = new ProgressDialog(worker);
+        progress.setWidth(500);
+        progress.setTitle("Importing Mirax files");
+
+        qupath.submitShortTask(worker);
+        progress.showAndWait();
 
         Map<ImageRetCode.IMAGE_CODE, Long> valcounts = retcodes.values().stream()
                 .collect(Collectors.groupingBy(v -> v, Collectors.counting()));
