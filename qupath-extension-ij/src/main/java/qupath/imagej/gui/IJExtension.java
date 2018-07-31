213f48868fe5fc014c13c8a30dfb095fa7e53b31
Tuatini Godard <tuatinigodard@gmail.com>
2018-07-31 12:15:26 +0200

    Remove IJ extention button

--- qupath-extension-ij/src/main/java/qupath/imagej/gui/IJExtension.java
+++ qupath-extension-ij/src/main/java/qupath/imagej/gui/IJExtension.java
@@ -407,8 +407,9 @@ public class IJExtension implements QuPathExtension {
 //					PathPrefs.setImageJPath(dir.getAbsolutePath());
 //			});
 //			popup.getItems().addAll(new SeparatorMenuItem(), miSetPluginsPath);
-			
-			qupath.addToolbarButton(btnImageJ);
+
+			// Remove for this version of QuPath
+			//qupath.addToolbarButton(btnImageJ);
 		} catch (Exception e) {
 			logger.error("Error adding toolbar buttons", e);
 			qupath.addToolbarCommand(commandExtractRegionCustom.getName(), commandExtractRegionCustom, PathIconFactory.createNode(QuPathGUI.iconSize, QuPathGUI.iconSize, PathIconFactory.PathIcons.EXTRACT_REGION));
