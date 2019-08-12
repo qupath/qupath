qupath/qupath

commit 07589ba396441fbb4eb689e38976d5e14a51235e

Specify cell detection channel by name, set channel names in scripts
This involves adding a new ImageData.updateServerMetadata(metadata) method that will also fire a property change.

qupath-core-processing/src/main/java/qupath/imagej/detect/cells/WatershedCellDetection.java
@@ -28,6 +28,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -65,6 +66,7 @@ import qupath.lib.analysis.stats.RunningStatistics;
 import qupath.lib.analysis.stats.StatisticsHelper;
 import qupath.lib.color.ColorDeconvolutionStains;
 import qupath.lib.color.StainVector;
+import qupath.lib.common.GeneralTools;
 import qupath.lib.images.ImageData;
 import qupath.lib.images.PathImage;
 import qupath.lib.images.servers.ImageChannel;
@@ -100,6 +102,8 @@ import qupath.lib.roi.interfaces.ROI;
  *
  */
 public class WatershedCellDetection extends AbstractTileableDetectionPlugin<BufferedImage> {
+	
+	private boolean parametersInitialized = false;
 
 	private static String[] micronParameters = {
 		"requestedPixelSizeMicrons",
@@ -121,8 +125,12 @@ public class WatershedCellDetection extends AbstractTileableDetectionPlugin<Buff
 		"cellExpansion",
 		};
 	
+//	private static String[] fluorescenceParameters = {
+//			"detectionImageFluorescence"
+//	};
+	
 	private static String[] fluorescenceParameters = {
-			"detectionImageFluorescence"
+			"detectionImage"
 	};
 
 	private static String[] brightfieldParameters = {
@@ -138,87 +146,6 @@ public class WatershedCellDetection extends AbstractTileableDetectionPlugin<Buff
 	static String IMAGE_HEMATOXYLIN = "Hematoxylin OD";
 	
 	ParameterList params;
-	
-	/**
-	 * Default constructor.
-	 */
-	public WatershedCellDetection() {
-		params = new ParameterList();
-		// TODO: Use a better way to determining if pixel size is available in microns
-//		params.addEmptyParameter("detectionParameters", "Detection parameters", true);
-
-		String microns = IJ.micronSymbol + "m";
-		
-		params.addTitleParameter("Setup parameters");
-
-		params.addIntParameter("detectionImageFluorescence", "Choose detection channel", 1, null, "Choose the channel number containing a nucleus counterstain (e.g. DAPI)");
-
-		params.addChoiceParameter("detectionImageBrightfield", "Choose detection image", IMAGE_HEMATOXYLIN, Arrays.asList(IMAGE_HEMATOXYLIN, IMAGE_OPTICAL_DENSITY),
-				"Transformed image to which to apply the detection");
-
-		params.addDoubleParameter("requestedPixelSizeMicrons", "Requested pixel size", .5, microns, 
-				"Choose pixel size at which detection will be performed - higher values are likely to be faster, but may be less accurate; set <= 0 to use the full image resolution");
-//		params.addDoubleParameter("requestedPixelSize", "Requested downsample factor", 1, "");
-
-		
-		params.addTitleParameter("Nucleus parameters");
-		
-		params.addDoubleParameter("backgroundRadiusMicrons", "Background radius", 8, microns, 
-				"Radius for background estimation, should be > the largest nucleus radius, or <= 0 to turn off background subtraction");
-		params.addDoubleParameter("medianRadiusMicrons", "Median filter radius", 0, microns,
-				"Radius of median filter used to reduce image texture (optional)");
-		params.addDoubleParameter("sigmaMicrons", "Sigma", 1.5, microns,
-				"Sigma value for Gaussian filter used to reduce noise; increasing the value stops nuclei being fragmented, but may reduce the accuracy of boundaries");
-		params.addDoubleParameter("minAreaMicrons", "Minimum area", 10, microns+"^2",
-				"Detected nuclei with an area < minimum area will be discarded");
-		params.addDoubleParameter("maxAreaMicrons", "Maximum area", 400, microns+"^2",
-				"Detected nuclei with an area > maximum area will be discarded");
-
-		params.addDoubleParameter("backgroundRadius", "Background radius", 15, "px", 
-				"Radius for background estimation, should be > the largest nucleus radius, or <= 0 to turn off background subtraction");
-		params.addDoubleParameter("medianRadius", "Median filter radius", 0, "px",
-				"Radius of median filter used to reduce image texture (optional)");
-		params.addDoubleParameter("sigma", "Sigma", 3, "px",
-				"Sigma value for Gaussian filter used to reduce noise; increasing the value stops nuclei being fragmented, but may reduce the accuracy of boundaries");
-		params.addDoubleParameter("minArea", "Minimum area", 10, "px^2",
-				"Detected nuclei with an area < minimum area will be discarded");
-		params.addDoubleParameter("maxArea", "Maximum area", 1000, "px^2",
-				"Detected nuclei with an area > maximum area will be discarded");
-
-		params.addTitleParameter("Intensity parameters");
-		params.addDoubleParameter("threshold", "Threshold", 0.1, null,
-				"Intensity threshold - detected nuclei must have a mean intensity >= threshold");
-//		params.addDoubleParameter("threshold", "Threshold", 0.1, null, 0, 2.5,
-//				"Intensity threshold - detected nuclei must have a mean intensity >= threshold");
-		params.addDoubleParameter("maxBackground", "Max background intensity", 2, null,
-				"If background radius > 0, detected nuclei occurring on a background > max background intensity will be discarded");
-		
-//		params.addBooleanParameter("mergeAll", "Merge all", true);
-		params.addBooleanParameter("watershedPostProcess", "Split by shape", true,
-				"Split merged detected nuclei based on shape ('roundness')");
-		params.addBooleanParameter("excludeDAB", "Exclude DAB (membrane staining)", false,
-				"Set to 'true' if regions of high DAB staining should not be considered nuclei; useful if DAB stains cell membranes");
-		
-		
-		params.addTitleParameter("Cell parameters");
-
-		params.addDoubleParameter("cellExpansionMicrons", "Cell expansion", 5, microns, 0, 25,
-				"Amount by which to expand detected nuclei to approximate the full cell area");
-		params.addDoubleParameter("cellExpansion", "Cell expansion", 5, "px",
-				"Amount by which to expand detected nuclei to approximate the full cell area");
-		
-//		params.addBooleanParameter("limitExpansionByNucleusSize", "Limit cell expansion by nucleus size", false, "If checked, nuclei will not be expanded by more than their (estimated) smallest diameter in any direction - may give more realistic results for smaller, or 'thinner' nuclei");
-			
-		params.addBooleanParameter("includeNuclei", "Include cell nucleus", true,
-				"If cell expansion is used, optionally include/exclude the nuclei within the detected cells");
-		
-		
-		params.addTitleParameter("General parameters");
-		params.addBooleanParameter("smoothBoundaries", "Smooth boundaries", true,
-				"Smooth the detected nucleus/cell boundaries");
-		params.addBooleanParameter("makeMeasurements", "Make measurements", true,
-				"Add default shape & intensity measurements during detection");
-	}
 	
 	
 	static class CellDetector implements ObjectDetector<BufferedImage> {
@@ -322,7 +249,10 @@ public class WatershedCellDetection extends AbstractTileableDetectionPlugin<Buff
 					ImagePlus imp = pathImage.getImage();
 					for (int c = 1; c <= imp.getNChannels(); c++) {
 						String name = imageChannels.get(c-1).getName();
-						channels.put(name, imp.getStack().getProcessor(imp.getStackIndex(c, 0, 0)).convertToFloatProcessor());
+						if (channels.containsKey(name))
+							logger.warn("Channel with duplicate name '{}' - will be skipped", name);
+						else
+							channels.put(name, imp.getStack().getProcessor(imp.getStackIndex(c, 0, 0)).convertToFloatProcessor());
 //						channels.put("Channel " + c, imp.getStack().getProcessor(imp.getStackIndex(c, 0, 0)).convertToFloatProcessor());
 					}
 				}
@@ -330,14 +260,18 @@ public class WatershedCellDetection extends AbstractTileableDetectionPlugin<Buff
 				channelsCell.putAll(channels);
 				
 				// Try to get detection channel for fluorescence
-				int detectionChannel = 1;
-				if (!isBrightfield)
-					detectionChannel = params.getIntParameterValue("detectionImageFluorescence");
-				if (detectionChannel <= 0 || detectionChannel > imageChannels.size()) {
-					logger.warn("Unable to find specified Channel {} - will default to Channel 1", detectionChannel);
-					detectionChannel = 1;
+				String detectionChannelName;
+				if (!isBrightfield) {
+					detectionChannelName = (String)params.getChoiceParameterValue("detectionImage");
+					fpDetection = channels.get(detectionChannelName);
+//					detectionChannel = params.getIntParameterValue("detectionImageFluorescence");
 				}
-				fpDetection = channels.get(imageChannels.get(detectionChannel-1).getName());
+				else throw new IllegalArgumentException("No valid detection channel is selected!");
+//				if (detectionChannelName == null) {
+//					detectionChannelName = imageChannels.get(detectionChannel-1).getName();
+//					logger.warn("Unable to find specified Channel {} - will default to Channel 1", detectionChannel);
+//				}
+//				fpDetection = channels.get(detectionChannelName);
 			}
 			WatershedCellDetector detector2 = new WatershedCellDetector(fpDetection, channels, channelsCell, roi, pathImage);
 			
@@ -413,9 +347,122 @@ public class WatershedCellDetection extends AbstractTileableDetectionPlugin<Buff
 	}
 	
 	
+	private ParameterList buildParameterList(final ImageData<BufferedImage> imageData) { 
+			
+		ParameterList params = new ParameterList();
+		// TODO: Use a better way to determining if pixel size is available in microns
+//		params.addEmptyParameter("detectionParameters", "Detection parameters", true);
+
+		String microns = IJ.micronSymbol + "m";
+		
+		params.addTitleParameter("Setup parameters");
+
+		String defaultChannel = null;
+		List<String> channelNames = new ArrayList<>();
+		String[] nucleusGuesses = new String[] {"dapi", "hoechst", "nucleus", "nuclei", "nuclear", "hematoxylin", "haematoxylin"};
+		for (ImageChannel channel : imageData.getServer().getMetadata().getChannels()) {
+			String name = channel.getName();
+			channelNames.add(name);
+			if (defaultChannel == null) {
+				String lower = name.toLowerCase();
+				for (String guess : nucleusGuesses) {
+					if (lower.contains(guess))
+						defaultChannel = name;
+				}
+			}
+		}
+		if (defaultChannel == null)
+			defaultChannel = channelNames.get(0);
+		if (channelNames.size() != new HashSet<>(channelNames).size())
+			logger.warn("Image contains duplicate channel names! This may be confusing for detection and analysis.");
+		params.addChoiceParameter("detectionImage", "Detection channel", defaultChannel, channelNames, "Choose the channel that should be used for nucleus detection (e.g. DAPI)");
+
+		params.addChoiceParameter("detectionImageBrightfield", "Detection image", IMAGE_HEMATOXYLIN, Arrays.asList(IMAGE_HEMATOXYLIN, IMAGE_OPTICAL_DENSITY),
+				"Transformed image to which to apply the detection");
+
+		params.addDoubleParameter("requestedPixelSizeMicrons", "Requested pixel size", .5, microns, 
+				"Choose pixel size at which detection will be performed - higher values are likely to be faster, but may be less accurate; set <= 0 to use the full image resolution");
+//		params.addDoubleParameter("requestedPixelSize", "Requested downsample factor", 1, "");
+
+		
+		params.addTitleParameter("Nucleus parameters");
+		
+		params.addDoubleParameter("backgroundRadiusMicrons", "Background radius", 8, microns, 
+				"Radius for background estimation, should be > the largest nucleus radius, or <= 0 to turn off background subtraction");
+		params.addDoubleParameter("medianRadiusMicrons", "Median filter radius", 0, microns,
+				"Radius of median filter used to reduce image texture (optional)");
+		params.addDoubleParameter("sigmaMicrons", "Sigma", 1.5, microns,
+				"Sigma value for Gaussian filter used to reduce noise; increasing the value stops nuclei being fragmented, but may reduce the accuracy of boundaries");
+		params.addDoubleParameter("minAreaMicrons", "Minimum area", 10, microns+"^2",
+				"Detected nuclei with an area < minimum area will be discarded");
+		params.addDoubleParameter("maxAreaMicrons", "Maximum area", 400, microns+"^2",
+				"Detected nuclei with an area > maximum area will be discarded");
+
+		params.addDoubleParameter("backgroundRadius", "Background radius", 15, "px", 
+				"Radius for background estimation, should be > the largest nucleus radius, or <= 0 to turn off background subtraction");
+		params.addDoubleParameter("medianRadius", "Median filter radius", 0, "px",
+				"Radius of median filter used to reduce image texture (optional)");
+		params.addDoubleParameter("sigma", "Sigma", 3, "px",
+				"Sigma value for Gaussian filter used to reduce noise; increasing the value stops nuclei being fragmented, but may reduce the accuracy of boundaries");
+		params.addDoubleParameter("minArea", "Minimum area", 10, "px^2",
+				"Detected nuclei with an area < minimum area will be discarded");
+		params.addDoubleParameter("maxArea", "Maximum area", 1000, "px^2",
+				"Detected nuclei with an area > maximum area will be discarded");
+
+		params.addTitleParameter("Intensity parameters");
+		params.addDoubleParameter("threshold", "Threshold", 0.1, null,
+				"Intensity threshold - detected nuclei must have a mean intensity >= threshold");
+//		params.addDoubleParameter("threshold", "Threshold", 0.1, null, 0, 2.5,
+//				"Intensity threshold - detected nuclei must have a mean intensity >= threshold");
+		params.addDoubleParameter("maxBackground", "Max background intensity", 2, null,
+				"If background radius > 0, detected nuclei occurring on a background > max background intensity will be discarded");
+		
+//		params.addBooleanParameter("mergeAll", "Merge all", true);
+		params.addBooleanParameter("watershedPostProcess", "Split by shape", true,
+				"Split merged detected nuclei based on shape ('roundness')");
+		params.addBooleanParameter("excludeDAB", "Exclude DAB (membrane staining)", false,
+				"Set to 'true' if regions of high DAB staining should not be considered nuclei; useful if DAB stains cell membranes");
+		
+		
+		params.addTitleParameter("Cell parameters");
+
+		params.addDoubleParameter("cellExpansionMicrons", "Cell expansion", 5, microns, 0, 25,
+				"Amount by which to expand detected nuclei to approximate the full cell area");
+		params.addDoubleParameter("cellExpansion", "Cell expansion", 5, "px",
+				"Amount by which to expand detected nuclei to approximate the full cell area");
+		
+//		params.addBooleanParameter("limitExpansionByNucleusSize", "Limit cell expansion by nucleus size", false, "If checked, nuclei will not be expanded by more than their (estimated) smallest diameter in any direction - may give more realistic results for smaller, or 'thinner' nuclei");
+			
+		params.addBooleanParameter("includeNuclei", "Include cell nucleus", true,
+				"If cell expansion is used, optionally include/exclude the nuclei within the detected cells");
+		
+		
+		params.addTitleParameter("General parameters");
+		params.addBooleanParameter("smoothBoundaries", "Smooth boundaries", true,
+				"Smooth the detected nucleus/cell boundaries");
+		params.addBooleanParameter("makeMeasurements", "Make measurements", true,
+				"Add default shape & intensity measurements during detection");
+		
+		return params;
+	}
+	
+	@Override
+	protected boolean parseArgument(ImageData<BufferedImage> imageData, String arg) {
+		if (arg != null) {
+			// We don't want running with old scripts to silently produce the wrong result, so instead we check
+			Map<String, String> map = GeneralTools.parseArgStringValues(arg);
+			if (map.containsKey("detectionImageFluorescence"))
+				throw new IllegalArgumentException("'detectionImageFluorescence' is not supported in this version of QuPath - use 'detectionImage' instead");
+		}
+		return super.parseArgument(imageData, arg);
+	}
 
 	@Override
 	public ParameterList getDefaultParameterList(final ImageData<BufferedImage> imageData) {
+		
+		if (!parametersInitialized) {
+			params = buildParameterList(imageData);
+		}
 		
 		// Show/hide parameters depending on whether the pixel size is known
 		Map<String, Parameter<?>> map = params.getParameters();

qupath-core/src/main/java/qupath/lib/images/ImageData.java
@@ -28,6 +28,7 @@ import java.beans.PropertyChangeSupport;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -36,6 +37,7 @@ import qupath.lib.color.ColorDeconvolutionStains;
 import qupath.lib.color.ColorDeconvolutionStains.DefaultColorDeconvolutionStains;
 import qupath.lib.common.GeneralTools;
 import qupath.lib.images.servers.ImageServer;
+import qupath.lib.images.servers.ImageServerMetadata;
 import qupath.lib.images.servers.ServerTools;
 import qupath.lib.objects.hierarchy.PathObjectHierarchy;
 import qupath.lib.objects.hierarchy.events.PathObjectHierarchyEvent;
@@ -218,6 +220,19 @@ public class ImageData<T> implements WorkflowListener, PathObjectHierarchyListen
 		changes = true;
 	}
 	
+	
+	/**
+	 * Update the ImageServer metadata. The benefit of using this method rather than manipulating 
+	 * the ImageServer directly is that it will fire a property change.
+	 * @param newMetadata
+	 */
+	public void updateServerMetadata(ImageServerMetadata newMetadata) {
+		Objects.requireNonNull(newMetadata);
+		var oldMetadata = server.getMetadata();
+		server.setMetadata(newMetadata);
+		pcs.firePropertyChange("serverMetadata", oldMetadata, newMetadata);
+		changes = changes || !oldMetadata.equals(newMetadata);
+	}
 	
 //	public void setColorDeconvolutionStains(final String stainsString) {
 //		setColorDeconvolutionStains(ColorDeconvolutionStains.parseColorDeconvolutionStainsArg(stainsString));

qupath-core/src/main/java/qupath/lib/scripting/QP.java
@@ -48,7 +48,9 @@ import qupath.lib.color.ColorDeconvolutionStains;
 import qupath.lib.common.ColorTools;
 import qupath.lib.images.ImageData;
 import qupath.lib.images.ImageData.ImageType;
+import qupath.lib.images.servers.ImageChannel;
 import qupath.lib.images.servers.ImageServer;
+import qupath.lib.images.servers.ImageServerMetadata;
 import qupath.lib.objects.PathObject;
 import qupath.lib.objects.PathObjects;
 import qupath.lib.objects.PathAnnotationObject;
@@ -509,6 +511,121 @@ public class QP {
 		if (selected instanceof TMACoreObject)
 			hierarchy.getSelectionModel().setSelectedObject(null);
 	}
+	
+	/**
+	 * Set the channel names for the current ImageData.
+	 * 
+	 * @param names
+	 * @see #setChannelNames(ImageData, String...)
+	 */
+	public static void setChannelNames(String... names) {
+		setChannelNames(getCurrentImageData(), names);
+	}
+	
+	/**
+	 * Set the channel names for the specified ImageData.
+	 * It is not essential to pass names for all channels: 
+	 * by passing n values, the first n channel names will be set.
+	 * Any name that is null will be left unchanged.
+	 * 
+	 * @param imageData
+	 * @param names
+	 */
+	public static void setChannelNames(ImageData<?> imageData, String... names) {
+		List<ImageChannel> oldChannels = imageData.getServer().getMetadata().getChannels();
+		List<ImageChannel> newChannels = new ArrayList<>(oldChannels);
+		for (int i = 0; i < names.length; i++) {
+			String name = names[i];
+			if (name == null)
+				continue;
+			newChannels.set(i, ImageChannel.getInstance(name, newChannels.get(i).getColor()));
+			if (i >= newChannels.size()) {
+				logger.warn("Too many channel names specified, only {} of {} will be used", newChannels.size(), names.length);
+				break;
+			}
+		}
+		setChannels(imageData, newChannels.toArray(ImageChannel[]::new));
+	}
+	
+	/**
+	 * Set the channel colors for the current ImageData.
+	 * 
+	 * @param colors
+	 * @see #setChannelColors(ImageData, Integer...)
+	 * @see #setChannelNames(ImageData, String...)
+	 */
+	public static void setChannelColors(Integer... colors) {
+		setChannelColors(getCurrentImageData(), colors);
+	}
+	
+	/**
+	 * Set the channel colors for the specified ImageData.
+	 * It is not essential to pass names for all channels: 
+	 * by passing n values, the first n channel names will be set.
+	 * Any name that is null will be left unchanged.
+	 * 
+	 * @param colors
+	 * @see #setChannelNames(ImageData, String...)
+	 */
+	public static void setChannelColors(ImageData<?> imageData, Integer... colors) {
+		List<ImageChannel> oldChannels = imageData.getServer().getMetadata().getChannels();
+		List<ImageChannel> newChannels = new ArrayList<>(oldChannels);
+		for (int i = 0; i < colors.length; i++) {
+			Integer color = colors[i];
+			if (color == null)
+				continue;
+			newChannels.set(i, ImageChannel.getInstance(newChannels.get(i).getName(), color));
+			if (i >= newChannels.size()) {
+				logger.warn("Too many channel colors specified, only {} of {} will be used", newChannels.size(), colors.length);
+				break;
+			}
+		}
+		setChannels(imageData, newChannels.toArray(ImageChannel[]::new));
+	}
+	
+	/**
+	 * Set the channels for the current ImageData.
+	 * 
+	 * @param channels
+	 * @see #setChannels(ImageData, ImageChannel...)
+	 */
+	public static void setChannels(ImageChannel... channels) {
+		setChannels(getCurrentImageData(), channels);
+	}
+	
+	/**
+	 * Set the channels for the specified ImageData.
+	 * Note that number of channels provided must match the number of channels of the current image.
+	 * <p>
+	 * Also, currently it is not possible to set channels for RGB images - attempting to do so 
+	 * will throw an IllegalArgumentException.
+	 * 
+	 * @param channels
+	 * @see #setChannelNames(ImageData, String...)
+	 * @see #setChannelColors(ImageData, Integer...)
+	 */
+	public static void setChannels(ImageData<?> imageData, ImageChannel... channels) {
+		ImageServer<?> server = imageData.getServer();
+		if (server.isRGB()) {
+			throw new IllegalArgumentException("Cannot set channels for RGB images");
+		}
+		List<ImageChannel> oldChannels = server.getMetadata().getChannels();
+		List<ImageChannel> newChannels = Arrays.asList(channels);
+		if (oldChannels.equals(newChannels)) {
+			logger.trace("Setting channels to the same values (no changes)");
+			return;
+		}
+		if (oldChannels.size() != newChannels.size())
+			throw new IllegalArgumentException("Cannot set channels - require " + oldChannels.size() + " channels but you provided " + channels.length);
+		
+		// Set the metadata
+		var metadata = server.getMetadata();
+		var metadata2 = new ImageServerMetadata.Builder(metadata)
+				.channels(newChannels)
+				.build();
+		imageData.updateServerMetadata(metadata2);
+	}
+	
 	
 	/**
 	 * Run the specified plugin on the current {@code ImageData}.

qupath-extension-script-editor/src/main/java/qupath/lib/gui/scripting/richtextfx/RichScriptEditor.java
@@ -200,7 +200,7 @@ public class RichScriptEditor extends DefaultScriptEditor {
 			
 			var cleanup = codeArea
 					.multiPlainChanges()
-					.successionEnds(Duration.ofMillis(500))
+					.successionEnds(Duration.ofMillis(250))
 					.supplyTask(() -> computeHighlightingAsync(codeArea.getText()))
 					.awaitLatest(codeArea.multiPlainChanges())
 					.filterMap(t -> {

qupath-gui-fx/src/main/java/qupath/lib/display/ImageDisplay.java
@@ -275,8 +275,23 @@ public class ImageDisplay extends AbstractImageRenderer {
 					tempChannelOptions.add(new ChannelDisplayInfo.DirectServerChannelInfo(imageData, c));
 				}
 			}
-		} else
+		} else {
+			// Ensure channel colors are set
+			boolean colorsUpdated = false;
+			for (int c = 0; c < channelOptions.size(); c++) {
+				var option = channelOptions.get(c);
+				if (option instanceof DirectServerChannelInfo && c < server.nChannels()) {
+					var channel = server.getChannel(c);
+					if (option.getColor() != channel.getColor()) {
+						((DirectServerChannelInfo)option).setLUTColor(channel.getColor());
+						colorsUpdated = true;
+					}
+				}
+			}
 			tempChannelOptions.addAll(channelOptions);
+			if (colorsUpdated)
+				saveChannelColorProperties();
+		}
 		
 		// Select all the channels
 		if (serverChanged) {

commit ffbd85b3f5ed36afbc71d975c1254f00c319f32c

Positive cell detection fixes
Fixed regression from https://github.com/petebankhead/qupath/commit/07589ba396441fbb4eb689e38976d5e14a51235e that resulted in an exception from the PostiveCellDetection constructor.

Added positive cell detection support for stains other than DAB.

qupath-core-processing/src/main/java/qupath/imagej/detect/cells/PositiveCellDetection.java
@@ -25,9 +25,13 @@ package qupath.imagej.detect.cells;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
 
+import qupath.lib.color.ColorDeconvolutionStains;
 import qupath.lib.images.ImageData;
 import qupath.lib.objects.PathObject;
 import qupath.lib.objects.classes.PathClassFactory;
@@ -48,17 +52,49 @@ public class PositiveCellDetection extends WatershedCellDetection {
 	 */
 	public PositiveCellDetection() {
 		super();
-		params.addTitleParameter("Intensity threshold parameters");
-		params.addChoiceParameter("thresholdCompartment", "Score compartment", "Nucleus: DAB OD mean",
-				Arrays.asList("Nucleus: DAB OD mean", "Nucleus: DAB OD max",
-						"Cytoplasm: DAB OD mean", "Cytoplasm: DAB OD max",
-						"Cell: DAB OD mean", "Cell: DAB OD max"));
-		params.addDoubleParameter("thresholdPositive1", "Threshold 1+", 0.2, null, 0, 1.5, "Low positive intensity threshold");
-		params.addDoubleParameter("thresholdPositive2", "Threshold 2+", 0.4, null, 0, 1.5, "Moderate positive intensity threshold");
-		params.addDoubleParameter("thresholdPositive3", "Threshold 3+", 0.6, null, 0, 1.5, "High positive intensity threshold");
-		params.addBooleanParameter("singleThreshold", "Single threshold", true);
 	}
 	
+	@Override
+	public ParameterList getDefaultParameterList(final ImageData<BufferedImage> imageData) {
+		if (parametersInitialized)
+			return super.getDefaultParameterList(imageData);
+		else {
+			super.getDefaultParameterList(imageData);
+			params.addTitleParameter("Intensity threshold parameters");
+			var stains = imageData.getColorDeconvolutionStains();
+			Set<String> channels = new LinkedHashSet<>();
+			if (stains != null) {
+				for (int i = 1; i <= 3; i++) {
+					var stain = stains.getStain(i);
+					if (!ColorDeconvolutionStains.isHematoxylin(stain) && !stain.isResidual())
+						channels.add(stain.getName() + " OD");
+				}
+			} else {
+				var server = imageData.getServer();
+				for (var channel : server.getMetadata().getChannels())
+					channels.add(channel.getName());
+			}
+			List<String> choices = new ArrayList<>();
+			for (var channel : channels) {
+				choices.add("Nucleus: " + channel + " mean");
+				choices.add("Nucleus: " + channel + " max");
+				choices.add("Cytoplasm: " + channel + " mean");
+				choices.add("Cytoplasm: " + channel + " max");
+				choices.add("Cell: " + channel + " mean");
+				choices.add("Cell: " + channel + " max");
+			}
+			params.addChoiceParameter("thresholdCompartment", "Score compartment", choices.get(0), choices, "Select the intensity measurement to threshold");
+//			params.addChoiceParameter("thresholdCompartment", "Score compartment", "Nucleus: DAB OD mean",
+//					Arrays.asList("Nucleus: DAB OD mean", "Nucleus: DAB OD max",
+//							"Cytoplasm: DAB OD mean", "Cytoplasm: DAB OD max",
+//							"Cell: DAB OD mean", "Cell: DAB OD max"));
+			params.addDoubleParameter("thresholdPositive1", "Threshold 1+", 0.2, null, 0, 1.5, "Low positive intensity threshold");
+			params.addDoubleParameter("thresholdPositive2", "Threshold 2+", 0.4, null, 0, 1.5, "Moderate positive intensity threshold");
+			params.addDoubleParameter("thresholdPositive3", "Threshold 3+", 0.6, null, 0, 1.5, "High positive intensity threshold");
+			params.addBooleanParameter("singleThreshold", "Single threshold", true);
+		}
+		return params;
+	}
 	
 	@Override
 	public String getName() {

qupath-core-processing/src/main/java/qupath/imagej/detect/cells/WatershedCellDetection.java
@@ -103,7 +103,7 @@ import qupath.lib.roi.interfaces.ROI;
  */
 public class WatershedCellDetection extends AbstractTileableDetectionPlugin<BufferedImage> {
 	
-	private boolean parametersInitialized = false;
+	protected boolean parametersInitialized = false;
 
 	private static String[] micronParameters = {
 		"requestedPixelSizeMicrons",

qupath-core/src/main/java/qupath/lib/projects/DefaultProject.java
@@ -327,7 +327,6 @@ class DefaultProject implements Project<BufferedImage> {
 	
 	@Override
 	public synchronized void syncChanges() throws IOException {
-		System.err.println("Syncing changes");
 		writeProject(getFile());
 		writePathClasses(pathClasses);
 //		if (file.isDirectory())