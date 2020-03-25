qupath/qupath

commit 9c95e313862aec74d187d60e47d759e8aed70f23

Cache PathClass.toString() and point centroids

qupath-core/src/main/java/qupath/lib/objects/classes/PathClass.java
@@ -47,6 +47,11 @@ public class PathClass implements Comparable<PathClass>, Serializable {
 	private final PathClass parentClass;
 	private final String name;
 	private Integer colorRGB;
+	
+	/**
+	 * Cached String representation
+	 */
+	private transient String stringRep = null;
 
 	PathClass() {
 		parentClass = null;
@@ -179,12 +184,15 @@ public class PathClass implements Comparable<PathClass>, Serializable {
 	
 	@Override
 	public String toString() {
-		if (name == null)
-			return defaultName;
-		if (isDerivedClass())
-			return derivedClassToString(parentClass, name);
-		else
-			return name;
+		if (stringRep == null) {
+			if (name == null)
+				stringRep = defaultName;
+			if (isDerivedClass())
+				stringRep = derivedClassToString(parentClass, name);
+			else
+				stringRep = name;
+		}
+		return stringRep;
 	}
 	
 	/**

qupath-core/src/main/java/qupath/lib/objects/PathObject.java
@@ -608,8 +608,8 @@ public abstract class PathObject implements Externalizable {
 	/**
 	 * Get a collection containing all descendant objects.
 	 * 
-	 * @param descendants optional collection to which the descandents should be added
-	 * @return collection containing all descendant object (the same as {@code descandents} if provided)
+	 * @param descendants optional collection to which the descendants should be added
+	 * @return collection containing all descendant object (the same as {@code descendants} if provided)
 	 */
 	public synchronized Collection<PathObject> getDescendantObjects(Collection<PathObject> descendants) {
 		if (childList == null || childList.isEmpty())

qupath-core/src/main/java/qupath/lib/roi/PointsROI.java
@@ -53,6 +53,7 @@ public class PointsROI extends AbstractPathROI implements Serializable {
 //	protected double pointRadius = -1;
 	
 	transient private double xMin = Double.NaN, yMin = Double.NaN, xMax = Double.NaN, yMax = Double.NaN;
+	transient private double xCentroid = Double.NaN, yCentroid = Double.NaN;
 	transient private ROI convexHull = null;
 //	transient protected Point2 pointAdjusting = null;
 	
@@ -110,20 +111,31 @@ public class PointsROI extends AbstractPathROI implements Serializable {
 	public double getCentroidX() {
 		if (points.isEmpty())
 			return Double.NaN;
-		double xSum = 0;
-		for (Point2 p : points)
-			xSum += p.getX();
-		return xSum / points.size();
+		if (Double.isNaN(xCentroid))
+			computeCentroid();
+		return xCentroid;
 	}
 
 	@Override
 	public double getCentroidY() {
 		if (points.isEmpty())
 			return Double.NaN;
+		if (Double.isNaN(yCentroid))
+			computeCentroid();
+		return yCentroid;
+	}
+	
+	
+	private void computeCentroid() {
+		double xSum = 0;
 		double ySum = 0;
-		for (Point2 p : points)
-			ySum += p.getY();
-		return ySum / points.size();
+		int n = points.size();
+		for (Point2 p : points) {
+			xSum += p.getX() / n;
+			ySum += p.getY() / n;
+		}
+		xCentroid = xSum;
+		yCentroid = ySum;
 	}
 
 

commit 052876fd6dead55f04a2438d67b8e33a51b0a18d

Fixed regression in PathClass.toString()
Problem introduced in https://github.com/qupath/qupath/commit/9c95e313862aec74d187d60e47d759e8aed70f23
This manifest itself in the default 'unclassified' PathClass being lost when reloading classifications from a project.

qupath-core/src/main/java/qupath/lib/objects/classes/PathClass.java
@@ -187,7 +187,7 @@ public class PathClass implements Comparable<PathClass>, Serializable {
 		if (stringRep == null) {
 			if (name == null)
 				stringRep = defaultName;
-			if (isDerivedClass())
+			else if (isDerivedClass())
 				stringRep = derivedClassToString(parentClass, name);
 			else
 				stringRep = name;