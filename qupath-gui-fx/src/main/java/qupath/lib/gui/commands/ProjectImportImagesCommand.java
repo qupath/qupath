d8572a0b25f52a8cc827ba77d373ea23ea4613b3
pete <pete.bankhead@gmail.com>
2020-05-04 16:50:01 +0100

    Limit thread pool size when importing images

--- qupath-gui-fx/src/main/java/qupath/lib/gui/commands/ProjectImportImagesCommand.java
+++ qupath-gui-fx/src/main/java/qupath/lib/gui/commands/ProjectImportImagesCommand.java
@@ -273,7 +273,10 @@ class ProjectImportImagesCommand {
 
 				updateMessage("Checking for compatible image readers...");
 				
-				var pool = Executors.newCachedThreadPool(ThreadTools.createThreadFactory("project-import", true));
+				// Limit the size of the thread pool
+				// The previous use of a cached thread pool caused trouble when importing may large, non-pyramidal images
+				var pool = Executors.newFixedThreadPool(PathPrefs.numCommandThreadsProperty().get(), ThreadTools.createThreadFactory("project-import", true));
+//				var pool = Executors.newCachedThreadPool(ThreadTools.createThreadFactory("project-import", true));
 				List<Future<List<ServerBuilder<BufferedImage>>>> results = new ArrayList<>();
 				List<ProjectImageEntry<BufferedImage>> projectImages = new ArrayList<>();
 				for (var item : items) {
