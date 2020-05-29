d5a11e5d06bb23f6d2487215ee730952ba239279
pete <pete.bankhead@gmail.com>
2020-05-29 18:13:53 +0100

    Add license header to recent files
    Files without an existing header were created more recently.

--- qupath-core/src/main/java/qupath/lib/images/servers/TileRequest.java
+++ qupath-core/src/main/java/qupath/lib/images/servers/TileRequest.java
@@ -1,3 +1,24 @@
+/*-
+ * #%L
+ * This file is part of QuPath.
+ * %%
+ * Copyright (C) 2018 - 2020 QuPath developers, The University of Edinburgh
+ * %%
+ * QuPath is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * QuPath is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with QuPath.  If not, see <https://www.gnu.org/licenses/>.
+ * #L%
+ */
+
 package qupath.lib.images.servers;
 
 import java.util.Collection;
