6784de7332fc387876f27ae142c5c7ac545b2718
Pete <pete.bankhead@gmail.com>
2022-05-31 18:16:14 +0100

    Use ImageJ for BufferedImageTools.resize
    This is a step towards removing OpenCV as a core dependency, see https://github.com/qupath/qupath/issues/961

--- qupath-core/src/main/java/qupath/lib/awt/common/BufferedImageTools.java
+++ qupath-core/src/main/java/qupath/lib/awt/common/BufferedImageTools.java
@@ -32,14 +32,11 @@ import java.awt.image.DataBuffer;
 import java.awt.image.WritableRaster;
 import java.util.Hashtable;
 
-import org.bytedeco.javacpp.indexer.FloatIndexer;
-import org.bytedeco.opencv.global.opencv_core;
-import org.bytedeco.opencv.global.opencv_imgproc;
-import org.bytedeco.opencv.opencv_core.Mat;
-import org.bytedeco.opencv.opencv_core.Size;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import ij.process.FloatProcessor;
+import ij.process.ImageProcessor;
 import qupath.lib.common.ColorTools;
 import qupath.lib.common.GeneralTools;
 import qupath.lib.images.servers.AbstractTileableImageServer;
@@ -422,8 +419,11 @@ public final class BufferedImageTools {
 	 * @param img input image to be resized
 	 * @param finalWidth target output width
 	 * @param finalHeight target output height
-	 * @param smoothInterpolate if true, the resize method is permitted to use a smooth interpolation method. If false, nearest-neighbor interpolation is used.
+	 * @param smoothInterpolate if true, the resize method is permitted to use a smooth interpolation method.
+	 *           If false, nearest-neighbor interpolation is used.
 	 * @return resized image
+	 * @implNote the method of smooth interpolation is not currently specified or adjustable.
+	 *           Since v0.4.0, it uses ImageJ's bilinear interpolation with area averaging - however this may change.
 	 */
 	public static BufferedImage resize(final BufferedImage img, final int finalWidth, final int finalHeight, boolean smoothInterpolate) {
 
@@ -444,42 +444,32 @@ public final class BufferedImageTools {
 		WritableRaster raster = img.getRaster();
 		WritableRaster raster2 = raster.createCompatibleWritableRaster(finalWidth, finalHeight);
 
+		boolean isIntType = isIntType(raster.getTransferType());
 		int w = img.getWidth();
 		int h = img.getHeight();
 		
-		int cvType = opencv_core.CV_32F;
-		boolean isIntType = isIntType(img.getRaster().getTransferType());
+		boolean areaAveraging;
+
+		var fp = new FloatProcessor(w, h);
+		if (smoothInterpolate) {
+			fp.setInterpolationMethod(ImageProcessor.BILINEAR);
+			areaAveraging = true;
+		} else {
+			fp.setInterpolationMethod(ImageProcessor.NEAREST_NEIGHBOR);
+			areaAveraging = false;
+		}
 		
-		Mat matInput = new Mat(h, w, cvType);
-		Size sizeOutput = new Size(finalWidth, finalHeight);
-		Mat matOutput = new Mat(sizeOutput, cvType);
-		FloatIndexer idxInput = matInput.createIndexer(true);
-		FloatIndexer idxOutput = matOutput.createIndexer(true);
-		float[] pixels = new float[w*h];
-		float[] pixelsOut = new float[finalWidth*finalHeight];
-		
-		int interp = smoothInterpolate ? opencv_imgproc.INTER_AREA : opencv_imgproc.INTER_NEAREST;
 		for (int b = 0; b < raster.getNumBands(); b++) {
+			float[] pixels = (float[])fp.getPixels();
 			raster.getSamples(0, 0, w, h, b, pixels);
-			idxInput.put(0L, pixels);
-			opencv_imgproc.resize(matInput, matOutput, sizeOutput, 0, 0, interp);
-			idxOutput.get(0, pixelsOut);
-			// If we have integer input, make sure we have integer output
-			// Fixes bug in QuPath v0.3 (and probably v0.2) where resizing a constant 
-			// region could result in new values due to subtle precision errors and 
-			// strict rounding down within raster.setSamples.
+			var fp2 = fp.resize(finalWidth, finalHeight, areaAveraging);
+			float[] pixelsOut = (float[])fp2.getPixels();
 			if (isIntType) {
 				for (int i = 0; i < pixelsOut.length; i++)
 					pixelsOut[i] = Math.round(pixelsOut[i]);
 			}
 			raster2.setSamples(0, 0, finalWidth, finalHeight, b, pixelsOut);
 		}
-		
-		idxInput.close();
-		idxOutput.close();
-		matInput.close();
-		matOutput.close();
-		sizeOutput.close();
 		
 //		System.err.println(String.format("Resizing from %d x %d to %d x %d", w, h, finalWidth, finalHeight));
 
