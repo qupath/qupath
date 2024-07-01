bd3542420bb9f97c92e8d22d4aab2e0daa5eef65
Pete <pete.bankhead@gmail.com>
2024-07-01 17:21:47 +0100

    Initial stain normalization support
    Support two types of 'normalization':
    - Subtract an offset, and scale the result (per channel) - intended for fluorescence
    - Apply color deconvolution, then re-convolve with standard stain vectors after optional channel rescaling - intended for brightfield
    
    Both methods are JSON-serializable, so can be used within projects.

---
+++ qupath-core/src/main/java/qupath/lib/images/servers/transforms/BufferedImageNormalizer.java
@@ -0,0 +1,43 @@
+/*-
+ * #%L
+ * This file is part of QuPath.
+ * %%
+ * Copyright (C) 2024 QuPath developers, The University of Edinburgh
+ * %%
+ * QuPath is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * QuPath is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with QuPath.  If not, see <https://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+package qupath.lib.images.servers.transforms;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * Interface for normalizing a BufferedImage.
+ * <p>
+ * Implementations should be stateless, thread-safe and JSON-serializable.
+ *
+ * @since v0.6.0
+ */
+public interface BufferedImageNormalizer {
+
+    /**
+     * Apply the normalization to the input image.
+     * Note that the input image may be modified in place.
+     * @param input
+     * @return
+     */
+    BufferedImage apply(BufferedImage input);
+
+}
