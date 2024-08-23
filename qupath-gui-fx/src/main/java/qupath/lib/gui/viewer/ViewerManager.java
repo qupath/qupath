04807606fc3a8ce00350063500846c9a9bd73f86
Pete <pete.bankhead@gmail.com>
2024-08-23 17:51:26 +0100

    Make viewer options persistent
    Addresses https://forum.image.sc/t/removing-the-cursor-location-ui-element-via-script/100008

--- qupath-gui-fx/src/main/java/qupath/lib/gui/viewer/ViewerManager.java
+++ qupath-gui-fx/src/main/java/qupath/lib/gui/viewer/ViewerManager.java
@@ -4,7 +4,7 @@
  * %%
  * Copyright (C) 2014 - 2016 The Queen's University of Belfast, Northern Ireland
  * Contact: IP Management (ipmanagement@qub.ac.uk)
- * Copyright (C) 2018 - 2023 QuPath developers, The University of Edinburgh
+ * Copyright (C) 2018 - 2024 QuPath developers, The University of Edinburgh
  * %%
  * QuPath is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
@@ -132,8 +132,8 @@ public class ViewerManager implements QuPathViewerListener {
 
 	private SplitPaneGrid splitPaneGrid;
 
-	private ViewerPlusDisplayOptions viewerDisplayOptions = new ViewerPlusDisplayOptions();
-	private OverlayOptions overlayOptions = new OverlayOptions();
+	private ViewerPlusDisplayOptions viewerDisplayOptions = ViewerPlusDisplayOptions.getSharedInstance();
+	private OverlayOptions overlayOptions = OverlayOptions.getSharedInstance();
 	
 	/**
 	 * Since v0.5.0, this uses a Reference so that we can potentially allow garbage collection is memory is scare
