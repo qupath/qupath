a33986e4937fe04cc9e56266226e2fb8e5faeb71
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2024-11-29 16:17:44 +0000

    Parameterise version in gradle and GHA

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,8 +10,9 @@ plugins {
 // Define project name
 rootProject.name = "qupath"
 
-// Define the current QuPath version
-var qupathVersion = "0.6.0-SNAPSHOT"
+// Define the current QuPath version (can be set as a gradle property too)
+var qupathVersion = findProperty("qupathVersion") ?: "0.6.0-SNAPSHOT"
+
 
 // Store version & derived app name in extra properties for build scripts to use
 gradle.extra["qupath.app.version"] = qupathVersion
@@ -66,7 +67,7 @@ dependencyResolutionManagement {
         // This can be useful to make custom QuPath builds with specific versions of extensions
         create("extraLibs") {
             library("djl", "io.github.qupath", "qupath-extension-djl").version("0.4.0-20240911.172830-2")
-            library("instanseg", "io.github.qupath", "qupath-extension-instanseg").version("0.0.1-20241020.174720-4")
+//            library("instanseg", "io.github.qupath", "qupath-extension-instanseg").version("0.0.1-20241020.174720-4")
             library("training", "io.github.qupath", "qupath-extension-training").version("0.0.1-20241022.065038-2")
             library("py4j", "io.github.qupath", "qupath-extension-py4j").version("0.1.0-20241021.201937-1")
             // Include or exclude bundled extensions
