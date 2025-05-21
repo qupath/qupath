8768c151a3a9749e22bb0a4457ccaa986c88c635
lleplat <lleplat@ed.ac.uk>
2025-05-21 11:16:38 +0100

    Add onTileWritten parameter in zarr writer

--- qupath-extension-bioformats/src/main/java/qupath/lib/images/writers/ome/zarr/OMEZarrWriter.java
+++ qupath-extension-bioformats/src/main/java/qupath/lib/images/writers/ome/zarr/OMEZarrWriter.java
@@ -30,6 +30,7 @@ import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 
 /**
  * Create an OME-Zarr file writer as described by version 0.4 of the specifications of the
@@ -48,6 +49,7 @@ public class OMEZarrWriter implements AutoCloseable {
     private final ImageServer<BufferedImage> server;
     private final Map<Integer, ZarrArray> levelArrays;
     private final ExecutorService executorService;
+    private final Consumer<TileRequest> onTileWritten;
 
     private OMEZarrWriter(Builder builder, String path) throws IOException {
         TransformedServerBuilder transformedServerBuilder = new TransformedServerBuilder(ImageServers.pyramidalizeTiled(
@@ -75,7 +77,7 @@ public class OMEZarrWriter implements AutoCloseable {
         if (builder.boundingBox != null) {
             transformedServerBuilder.crop(builder.boundingBox);
         }
-        server = transformedServerBuilder.build();
+        this.server = transformedServerBuilder.build();
 
         OMEZarrAttributesCreator attributes = new OMEZarrAttributesCreator(server.getMetadata());
 
@@ -85,14 +87,15 @@ public class OMEZarrWriter implements AutoCloseable {
         );
 
         OMEXMLCreator.create(server.getMetadata()).ifPresent(omeXML -> createOmeSubGroup(root, path, omeXML));
-        levelArrays = createLevelArrays(
+        this.levelArrays = createLevelArrays(
                 server,
                 root,
                 attributes.getLevelAttributes(),
                 builder.compressor
         );
 
-        executorService = Executors.newFixedThreadPool(builder.numberOfThreads);
+        this.executorService = Executors.newFixedThreadPool(builder.numberOfThreads);
+        this.onTileWritten = builder.onTileWritten;
     }
 
     /**
@@ -155,6 +158,9 @@ public class OMEZarrWriter implements AutoCloseable {
             } catch (Exception e) {
                 logger.error("Error when writing tile", e);
             }
+            if (onTileWritten != null) {
+                onTileWritten.accept(tileRequest);
+            }
         });
     }
 
@@ -189,6 +195,7 @@ public class OMEZarrWriter implements AutoCloseable {
         private int zEnd;
         private int tStart = 0;
         private int tEnd;
+        private Consumer<TileRequest> onTileWritten = null;
 
         /**
          * Create the builder.
@@ -325,6 +332,21 @@ public class OMEZarrWriter implements AutoCloseable {
         public Builder timePoints(int tStart, int tEnd) {
             this.tStart = tStart;
             this.tEnd = tEnd;
+            return this;
+        }
+
+        /**
+         * Set a function that will be called each time a {@link TileRequest} is successfully
+         * or unsuccessfully written.
+         * <p>
+         * This function may be called from any thread.
+         *
+         * @param onTileWritten a function that will be called each time a {@link TileRequest} is successfully
+         *                      or unsuccessfully written
+         * @return this builder
+         */
+        public Builder onTileWritten(Consumer<TileRequest> onTileWritten) {
+            this.onTileWritten = onTileWritten;
             return this;
         }
 
