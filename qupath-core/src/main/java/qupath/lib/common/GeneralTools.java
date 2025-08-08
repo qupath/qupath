5b48b87851d60ccfc0b5b2cbe23fd930ccca64cd
lleplat <lleplat@ed.ac.uk>
2025-08-08 14:42:41 +0100

    Address comments

--- qupath-core/src/main/java/qupath/lib/common/GeneralTools.java
+++ qupath-core/src/main/java/qupath/lib/common/GeneralTools.java
@@ -24,7 +24,6 @@
 package qupath.lib.common;
 
 import java.awt.Desktop;
-import java.awt.GraphicsEnvironment;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -766,18 +765,12 @@ public final class GeneralTools {
 	 * @param fileToDelete the file or empty directory to delete
 	 * @param preferTrash whether an attempt should be made to move the provided file/empty directory to trash before deleting it
 	 * @return true if the provided file/empty directory is successfully deleted, false otherwise
-	 * @throws java.awt.HeadlessException if the preferTrash parameter and {@link GraphicsEnvironment#isHeadless()} are true
-	 * @throws UnsupportedOperationException if the preferTrash parameter is true and the {@link Desktop} class is not supported
-	 * on the current platform
 	 * @throws NullPointerException if the provided file/empty directory is null
-	 * @throws SecurityException if the calling thread doesn't have the permission to read or delete the provided file/directory
-	 * @throws IllegalArgumentException if the provided file/directory doesn't exit
+	 * @throws IllegalArgumentException if the provided file/directory doesn't exist
 	 */
 	public static boolean deleteFile(File fileToDelete, boolean preferTrash) {
-		if (preferTrash && Desktop.isDesktopSupported()) {
-			var desktop = Desktop.getDesktop();
-			if (desktop.isSupported(Desktop.Action.MOVE_TO_TRASH) && moveToTrash(desktop, fileToDelete))
-				return true;
+		if (preferTrash && moveToTrash(fileToDelete)) {
+			return true;
 		}
 		return fileToDelete.delete();
 	}
@@ -785,33 +778,44 @@ public final class GeneralTools {
 	/**
 	 * Attempt to move the provided file or directory to trash. This may fail depending on the current platform.
 	 *
-	 * @param desktop the desktop to use when moving the provided file/directory to trash
-	 * @param fileToDelete the file or directory to move to trash
+	 * @param fileOrDirectory the file or directory to move to trash
 	 * @return whether the provided file/directory was moved to trash
-	 * @throws NullPointerException if one of the provided parameters is null
-	 * @throws SecurityException if the calling thread doesn't have the permission to read or move the provided file/directory
-	 * @throws UnsupportedOperationException if the provided desktop doesn't support the {@link Desktop.Action#MOVE_TO_TRASH} action
-	 * @throws IllegalArgumentException if the provided file/directory doesn't exit
+	 * @throws NullPointerException if the provided file/directory is null
+	 * @throws IllegalArgumentException if the provided file/directory doesn't exist
 	 */
-	public static boolean moveToTrash(Desktop desktop, File fileToDelete) {
+	public static boolean moveToTrash(File fileOrDirectory) {
+		if (Desktop.isDesktopSupported()) {
+			try {
+				var desktop = Desktop.getDesktop();
+				return desktop.isSupported(Desktop.Action.MOVE_TO_TRASH) && moveToTrash(desktop, fileOrDirectory);
+			} catch (UnsupportedOperationException e) {
+				logger.warn("Cannot move {} to trash", fileOrDirectory, e);
+				return false;
+			}
+		} else {
+			logger.warn("Desktop class not supported. Cannot move {} to trash", fileOrDirectory);
+			return false;
+		}
+	}
+
+	private static boolean moveToTrash(Desktop desktop, File fileOrDirectory) {
 		if (SwingUtilities.isEventDispatchThread() || !GeneralTools.isWindows()) {
 			// It seems safe to call move to trash from any thread on macOS and Linux
 			// We can't use the EDT on macOS because of https://bugs.openjdk.org/browse/JDK-8087465
-			return desktop.moveToTrash(fileToDelete);
+			return desktop.moveToTrash(fileOrDirectory);
 		} else {
 			// EXCEPTION_ACCESS_VIOLATION associated with moveToTrash reported on Windows 11.
 			// https://github.com/qupath/qupath/issues/1738
 			// Could not be replicated (but we didn't have Windows 11...); taking a guess that this might help.
 			try {
- 				SwingUtilities.invokeAndWait(() -> moveToTrash(desktop, fileToDelete));
+				SwingUtilities.invokeAndWait(() -> moveToTrash(desktop, fileOrDirectory));
 			} catch (Exception e) {
 				logger.error("Exception moving file to trash: {}", e.getMessage(), e);
 				return false;
 			}
-			return !fileToDelete.exists();
+			return !fileOrDirectory.exists();
 		}
 	}
-
 
 	/**
 	 * Read URL as String, with specified timeout in milliseconds.
